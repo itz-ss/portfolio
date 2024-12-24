41da2c724326b54315bb1223c179ecb8ae132fc7
Saransh Singh <rahulkushwaha.2205@gmail.com>
2024-12-24 20:23:33 +0530

    link add

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,18 @@
 import {FaLinkedin} from 'react-icons/fa';
 import {FaGithub} from 'react-icons/fa';
 import {FaSquareXTwitter} from 'react-icons/fa6';
-import {FaInstagram} from 'react-icons/fa';
-import { SiSession } from "react-icons/si";
+import { BsStripe } from "react-icons/bs";
 const Navbar = () => {
   return (
     <nav className='mb-20 flex items-center justify-between py-6 '>
         <div className='flex flex-shrink-0 item-center'>
-        <SiSession className='size-11 text-white' />
-        <SiSession className='size-11 text-white' />
+        <BsStripe className='size-9 text-white' />
+        <BsStripe className='size-9 text-white mx-1' />
         </div>
         <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-            <FaLinkedin />
-            <FaGithub/>
-            <FaSquareXTwitter/>
-            <FaInstagram/>
+            <a href='https://www.linkedin.com/in/saransh-singh-773164307/'><FaLinkedin /></a>
+            <a href='https://github.com/itz-ss'><FaGithub/></a>
+            <a href='https://x.com/saransh2205'><FaSquareXTwitter/></a>
         </div>
     </nav>
   )
