6669aeeae932b31d5650cb0ae4f3df61be851649
Saransh Singh <rahulkushwaha.2205@gmail.com>
2025-04-12 18:04:33 +0530

    changed pic

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -11,17 +11,20 @@ const Contact = () => {
       className="my-10 text-center text-4xl "> Get In Touch </motion.h1>
       <div className="text-center tracking-tighter">
         <motion.p
-         whileInView={{opacity: 1, x:0}}
-         initial={{opacity: 0, x: -100}}
-         transition={{duration: 1}}
+          whileInView={{opacity: 1, y:0}}
+          initial={{opacity: 0, y: 100}}
+          transition={{duration: 1}}
          className="my-4"> {CONTACT.address} </motion.p>
         <motion.p
-        whileInView={{opacity: 1, x:0}}
-        initial={{opacity: 0, x: -100}}
-        transition={{duration: 1}}
+         whileInView={{opacity: 1, y:0}}
+         initial={{opacity: 0, y: 100}}
+         transition={{duration: 1}}
          className="my-4"> {CONTACT.phoneNo } </motion.p>
-        <a
-        className="my-4 underline"> {CONTACT.email} </a>
+        <motion.p
+        whileInView={{opacity: 1, y:0}}
+        initial={{opacity: 0, y: 100}}
+        transition={{duration: 1}}
+        className="my-4 underline"> {CONTACT.email} </motion.p>
       </div>
     </div>
   )
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,12 +4,12 @@ import {FaSquareXTwitter} from 'react-icons/fa6';
 import { BsStripe } from "react-icons/bs";
 const Navbar = () => {
   return (
-    <nav className='mb-20 flex items-center justify-between py-6 '>
-        <div className='flex flex-shrink-0 item-center'>
+    <nav className=' mb-20 flex items-center justify-between py-6  '>
+        <div className='flex flex-shrink-0 item-center  '>
         <BsStripe className='size-9 text-white' />
         <BsStripe className='size-9 text-white mx-1' />
         </div>
-        <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <div className='m-8 flex items-center justify-center gap-4 text-2xl '>
             <a href='https://www.linkedin.com/in/saransh-singh-773164307/'><FaLinkedin /></a>
             <a href='https://github.com/itz-ss'><FaGithub/></a>
             <a href='https://x.com/saransh2205'><FaSquareXTwitter/></a>
